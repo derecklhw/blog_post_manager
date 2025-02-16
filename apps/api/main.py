60975b5a8cc908a617a896c0c80c4bd2e0dc6094
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2025-02-16 17:32:23 +0400

    fix: add a CORS middleware for Cross-Origin-Request blocked on frontend

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from router import posts
 from database import engine
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 
@@ -13,8 +27,3 @@ models.Base.metadata.create_all(bind=engine)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
